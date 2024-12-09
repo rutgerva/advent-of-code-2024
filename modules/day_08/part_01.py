471957379a61f978c391ecaaf7d913393f66b8ec
Rutger Van Acker <fioxxukutti@gmail.com>
2024-12-09 21:27:05 +0100

    Day 8, part1

--- modules/day_08/part_01.py
+++ modules/day_08/part_01.py
@@ -1,5 +1,6 @@
 """Code to solve part 01 of day 08"""
 import re
+import numpy as np
 def read_file(filename):
     """Methods reads a file and returns stream of lines"""
     all_lines = []
@@ -11,18 +12,29 @@ def read_file(filename):
     return all_lines
 
 def is_inbounds(x, y, grid):
-    return 0 < x < len(grid) and 0 < y < len(grid[0])
+    return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 def is_antenna(c):
     pattern = r'^[a-zA-Z0-9]$'
     return re.match(pattern, c)
 
-grid = read_file('example_input')
+grid = np.array(read_file('input'))
 antennas = list()
-for j in range(0, len(grid)):
-    for i in range(0, len(grid[j])):
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
         if is_antenna(grid[i][j]):
-            antennas.append([grid[i][j], i, j])
-[antenna in antennas if antenna[0] == '0']
+            antennas.append([grid[i][j], int(j), int(i)])
+antinodes = list()
+for i in range(len(antennas)):
+    frequency = antennas[i][0]
+    for antenna in antennas[i + 1:]:
+        if antenna[0] == frequency:
+            dist_x = antenna[1] - antennas[i][1]
+            dist_y = antenna[2] - antennas[i][2]
+            node_1_x = antennas[i][1] - dist_x
+            node_1_y = antennas[i][2] - dist_y
+            node_2_x = antenna[1] + dist_x
+            node_2_y = antenna[2] + dist_y
+            antinodes.append([node_1_x, node_1_y]) if is_inbounds(node_1_x, node_1_y, grid) and [node_1_x, node_1_y] not in antinodes else None
+            antinodes.append([node_2_x, node_2_y]) if is_inbounds(node_2_x, node_2_y, grid) and [node_2_x, node_2_y] not in antinodes else None
 
-
-print(antennas)
+print(len(antinodes))
